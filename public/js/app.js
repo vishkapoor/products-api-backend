91d925a6f68ff0543ceb3c4a509b7784370177a8
Vishal Kapoor <vishal.kapoor@ecdltd.co.uk>
2020-01-18 00:07:57 +0000

    frontend build

--- public/js/app.js
+++ public/js/app.js
@@ -52065,8 +52065,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/vishal/Sites/laravel-restful-api/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/vishal/Sites/laravel-restful-api/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/vish/sites/products-api-backend/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/vish/sites/products-api-backend/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
